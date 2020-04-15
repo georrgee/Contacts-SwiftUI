ff11863b6d5b541da6a9cf6e47907e74d8c06338
georrgee <garcia.george.b@gmail.com>
2020-04-15 16:49:55 -0700

    Initial Commit

---
+++ Contacts-Diffable-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Contacts-Diffable-SwiftUI
+//
+//  Created by George Garcia on 4/15/20.
+//  Copyright © 2020 GeeTeam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
