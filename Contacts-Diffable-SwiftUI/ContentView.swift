83b2942421adb898d2dc3583565316f162c75c60
georrgee <garcia.george.b@gmail.com>
2020-04-15 17:33:48 -0700

    Setting up TableViewDiffable

--- Contacts-Diffable-SwiftUI/ContentView.swift
+++ Contacts-Diffable-SwiftUI/ContentView.swift
@@ -1,21 +1,114 @@
-//
 //  ContentView.swift
 //  Contacts-Diffable-SwiftUI
-//
 //  Created by George Garcia on 4/15/20.
 //  Copyright © 2020 GeeTeam. All rights reserved.
-//
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+enum SectionType {
+    case yourself, family, closeFriends, friends
+}
+
+struct Contact: Hashable {
+    let name: String
+}
+
+class DiffableTableViewController: UITableViewController {
+    
+    lazy var source: UITableViewDiffableDataSource<SectionType, Contact> = .init(tableView: self.tableView) { (_, indexPath, contact) -> UITableViewCell? in
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = contact.name
+        return cell
     }
+    
+    private func setupSource() {
+        var snapshot = source.snapshot() //
+        snapshot.appendSections([.yourself, .family, .closeFriends, .friends])
+        
+        snapshot.appendItems(
+            [.init(name: "George"),
+        ], toSection: .yourself)
+
+        snapshot.appendItems(
+            [.init(name: "Mom"),
+             .init(name: "Dad"),
+             .init(name: "Lil Bro")
+        ], toSection: .family)
+        
+        snapshot.appendItems(
+            [.init(name: "Kevin"),
+             .init(name: "Checko"),
+             .init(name: "Rey"),
+             .init(name: "Stephen")
+        ], toSection: .closeFriends)
+        
+        snapshot.appendItems(
+            [.init(name: "Tien"),
+             .init(name: "Greg"),
+             .init(name: "Anna"),
+             .init(name: "CJ"),
+        ], toSection: .friends)
+        
+        source.apply(snapshot)
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        
+        switch section {
+        case 0:
+            label.text = "Yourself"
+        case 1:
+            label.text = "Family"
+        case 2:
+            label.text = "Close Friends"
+        case 3:
+            label.text = "Friends"
+        default:
+            label.text = "N/A"
+        }
+        
+        return label
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Contacts"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        setupSource()
+    }
+    
+}
+
+struct DiffiableContainer: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+        UINavigationController(rootViewController: DiffableTableViewController(style: .insetGrouped))
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        
+    }
+    
+    typealias UIViewControllerType = UIViewController
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        DiffiableContainer()
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Yo")
     }
 }
